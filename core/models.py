869699befe0fd7e6ac6be1718572f64acf1b203c
Samy Djemaï <53857555+SamyDjemai@users.noreply.github.com>
2020-11-07 14:53:56 +0100

    Added Follow model

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 
 class User(AbstractUser):
@@ -26,3 +27,22 @@ class Tweet(models.Model):
 
     def get_absolute_url(self):
         return reverse("tweet_detail", kwargs={"pk": self.pk})
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="followees"
+    )
+    followee = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="followers"
+    )
+
+    class Meta:
+        unique_together = ("follower", "followee")
+
+    def __str__(self):
+        return f"{self.follower} follows {self.followee}"
+
+    def clean(self):
+        if self.follower == self.followee:
+            raise ValidationError("You can't follow yourself!")
